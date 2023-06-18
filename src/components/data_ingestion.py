97f7fe7c86b42373f301aac4fef09fc418c26dec
ambikagovindan <ambikagovindan90@gmail.com>
2023-06-18 11:02:31 +0530

    'model_trainer'

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -48,9 +48,11 @@ if __name__=='__main__':
     obj = DataIngestion() 
     train_data,test_data = obj.initiate_data_ingestion()           
 '''
+'''
 
 if __name__=='__main__':
     obj = DataIngestion() 
     train_data_path,test_data_path = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+'''    
